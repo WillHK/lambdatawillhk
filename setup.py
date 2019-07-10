81027d0f3f07eb0bf97ecd41f179a7ac12b3aa21
Will Haeck <will.haeck@gmail.com>
2019-07-09 19:11:43 -0700

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='lambdatawillhk',
     packages=['lambdatawillhk'],
-    version='0.1',
+    version='0.1.1',
     license='MIT',
     description='collection of dataframe functions',
     author='Will Haeck',
